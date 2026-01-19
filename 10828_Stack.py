f72122a65396a341dbc3dcf3522d2ac3ac47c1c1
goldmung <gyu04170@gmail.com>
2026-01-20 00:00:59 +0900

    solve: BOJ 10828 stack

--- 10828_Stack.py
+++ 10828_Stack.py
@@ -7,11 +7,39 @@
 
 ## ❌ 시간초과 풀이 ##
 ## 원인 : 입출력 I/O 문제
+# stack = []
+# num = int(input())
+
+# for i in range(num):
+#     cmd = input().split()
+#     if cmd[0] == "push" :
+#         stack.append(int(cmd[1]))
+#     elif cmd[0] == "top":
+#         if stack:
+#             print(stack[-1])
+#         else: 
+#             print(-1)
+#     elif cmd[0] =="size":
+#         print(len(stack))
+#     elif cmd[0] =="empty":
+#         if stack :
+#             print(0)
+#         else: 
+#             print(1)
+#     elif cmd[0] == "pop":
+#         if stack :
+#             print(stack.pop())
+#         else:
+#             print(-1)
+
+## 개선 풀이 
+import sys
+
 stack = []
-num = int(input())
+num =int(sys.stdin.readline())
 
 for i in range(num):
-    cmd = input().split()
+    cmd = sys.stdin.readline().split()
     if cmd[0] == "push" :
         stack.append(int(cmd[1]))
     elif cmd[0] == "top":
@@ -31,5 +59,3 @@ for i in range(num):
             print(stack.pop())
         else:
             print(-1)
-
- ## 개선 풀이 
