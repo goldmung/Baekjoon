6bd8874d5a9d92a86f624e4231f4ec3f3f0dcee3
goldmung <gyu04170@gmail.com>
2026-01-22 23:12:55 +0900

    solve: BOJ Stack Sequence

--- 1874_Stack_sequence.py
+++ 1874_Stack_sequence.py
@@ -1,24 +1,26 @@
 '''
 알고리즘 : Stack
 '''
-import sys
 
-n = int(sys.stdin.readline())
-stack =[]
-next_num = 1
+## ❌ 시간초과 풀이 ##
+# import sys
 
-for i in range(n):
-    temp = int(sys.stdin.readline().rstrip())
-    while next_num <= temp:
-        stack.append(next_num)
-        print('+')
-        next_num +=1  
-    if stack[-1] ==temp :
-        stack.pop()
-        print('-')
-    else: 
-        print("NO")
-        break;
+# n = int(sys.stdin.readline())
+# stack =[]
+# next_num = 1
+
+# for i in range(n):
+#     temp = int(sys.stdin.readline().rstrip())
+#     while next_num <= temp:
+#         stack.append(next_num)
+#         print('+')
+#         next_num +=1  
+#     if stack[-1] ==temp :
+#         stack.pop()
+#         print('-')
+#     else: 
+#         print("NO")
+#         break;
 
 # 문제 이해하는 데 시간 많이 소요함
 # 1. 입력 값들에 대한 의미 이해
@@ -27,3 +29,26 @@ for i in range(n):
 # 2. 외부에서 push 가능한 숫자
 # 1-2-3-4-5-6-7-8 순으로 
 # 3. 그럼 pop한 결과대로 stack의 결과가 나와야한다는거네
+
+## 개선 풀이 
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+result = []
+next_num = 1
+
+for _ in range(n):
+    temp = int(sys.stdin.readline())
+    while next_num <= temp:
+        stack.append(next_num)
+        result.append('+')
+        next_num += 1
+    if stack[-1] == temp:
+        stack.pop()
+        result.append('-')
+    else:
+        result = ["NO"]  ## 덮어쓰기
+        break
+
+sys.stdout.write('\n'.join(result) + '\n') ## 한번에 출력
